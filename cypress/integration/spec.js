cfb5dc06ed346ea1a13874669bd460d57833849f
Josh Olson <Olson.dev@Gmail.com>
2020-12-09 16:32:49 -0600

    Work in progress making an issue reproduction

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,12 @@
 /// <reference types="cypress" />
 describe('page', () => {
   it('works', () => {
-    cy.visit('https://example.cypress.io')
+    cy.intercept('GET', 'https://cataas.com/cat').as('cat')
+    cy.wait(2000) // For you to get a window over Cypress
+    cy.visit('./global-loading.html')
+    cy.get('#load-content').click()
+    cy.wait('@cat')
+    cy.get('#new-btn').click()
+    cy.contains('Hello Cypress!').should('be.visible')
   })
 })
